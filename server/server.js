1a7c24ba9e3bc1d2a6a3cb200e3b22154d5b94e8
Maurice Brown <mauricebrwn6890@gmail.com>
2022-12-11 17:44:42 -0600

    basic complete with css

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ let total ;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 5001;
+const PORT = 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended:true}))
 
