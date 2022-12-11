7529ea3c3004042c0d9176d9fbc8aa729f41c643
Maurice Brown <mauricebrwn6890@gmail.com>
2022-12-11 13:49:20 -0600

    submit equation function/clear function

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,75 @@
 $(document).ready(onReady);
 
 function onReady(){
-console.log('hi!')
+    //clickhandlers
+    $('#addButton').on('click', addition);
+    $('#minusButton').on('click', subtract);
+    $('#multiplyButton').on('click', multiply);
+    $('#divideButton').on('click', divide);
+    $('#EqualsButton').on('click', submitEquation);
+    $('#clearButton').on('click',onClear);
 
 }
+let add = '+'
+let minus = '-'
+let multiplication = '*'
+let division = '/'
+
+let inputArray = [
+    {
+        inputOne:'',
+        inputTwo:'',
+        calcOperator:'',
+        finalResult:''
+    }
+]
+
+function submitEquation() {
+    let firstInput = $('#numberOne').val()
+    let secondInput = $('#numberTwo').val()
+    inputArray.inputOne = firstInput
+    inputArray.inputTwo = secondInput
+    $.ajax({
+        method:'POST',
+        url:'/result',
+        data:inputArray[0]
+    }).then((response) => {
+
+    })
+    $.ajax({
+        method:'GET',
+        url: '/answer'
+    }).then((response) => {
+        $('#answer').text(response[response.length].finalResult)
+    })
+}
+
+//clear function//
+function onClear() {
+    $('#numberOne').val('');
+    $('#numberTwo').val('');
+   
+}
+
+function renderCalculation(array){
+    $('#calculationList').empty();
+    for(let i = 0; i< array.length; i++){
+        $('#calculation list').append(`
+        <li>${array[i].inputOne} ${array[i].calcOperator} ${array[i].finalResult}</li>
+        `)
+    }
+}
+
+function addition(){
+    inputArray[0].operator = add;
+}
+function subtract(){
+    inputArray[0].operator = minus;
+}
+function multiply(){
+    inputArray[0].operator = multiply;
+}
+function divide(){
+    inputArray[0].operator = divide;
+}
+
--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
+let inputArray = [];
+
+let total ;
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const PORT = 5000;
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended:true}))
 
+app.post('/result', (req, res) => {
+    let answerOne = req.body
+    if(answerOne.operator === "+") {
+        total = Number(answerOne.inputOne) + Number(answerOne)
+    }
+    else if(answerOne.operator === '-'){
+        total = Number(answerOne.inputOne) - Number(answerOne)
+    }
+    else if(answerOne.operator === '*'){
+        total = Number(answerOne.inputOne) * Number(answerOne)
+    }
+    else if(answerOne.operator === '/'){
+        total = Number(answerOne.inputOne) / Number(answerOne)
+    }
+    answerOne.finalResult = total
+    inputArray.push(answerOne);
+    res.sendStatus(201);
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/answer', (req, res) => {
+    res.send(inputArray);
+})
 
 
 app.listen(PORT,() => {
