bda0459f4690195fad7ffe0796974dc112dd63c9
Maurice Brown <mauricebrwn6890@gmail.com>
2022-12-11 17:16:07 -0600

    basic mode is complete

--- server/public/client.js
+++ server/public/client.js
@@ -1,22 +1,21 @@
-
-
 $(document).ready(onReady);
 
 function onReady(){
-    //clickhandlers
+    //clickhandlers for operation/clear and equates button
     $('#addButton').on('click', addition);
     $('#minusButton').on('click', subtract);
     $('#multiplyButton').on('click', multiply);
     $('#divideButton').on('click', divide);
     $('#equalsButton').on('click', submitEquation);
-    //$('#clearButton').on('click',onClear);
+    $('#clearButton').on('click',onClear);
 
 }
+//values for operation buttons
 let add = '+'
 let minus = '-'
 let multiplication = '*'
 let division = '/'
-
+//creating array inot object to pull values
 let inputArray = 
     {
         inputOne:'',
@@ -35,6 +34,7 @@ function sendIt(inputArray) {
 console.log(response);
     })
 }
+
 function submitEquation() {
     let firstInput = $('#numberOne').val()
     let secondInput = $('#numberTwo').val()
@@ -43,29 +43,15 @@ function submitEquation() {
     console.log(inputArray);
     sendIt(inputArray);
     renderCalculation();
+    appendList();
 }
-    //$.ajax({
-      //  method:'POST',
-        //url:'/result',
-        //data:inputArray
-    //}).then((response) => {
-
-    //})
-    // $.ajax({
-    //     method:'GET',
-    //     url: '/answer'
-    // }).then((response) => {
-    //     $('#answer').text(response[response.length - 1].finalResult)
-    // })
-
-
-//clear function//
-//function onClear() {
-  //$('#numberOne').val('');
-   //$('#numberTwo').val('');
+//clears out values in the inputs when pressing c button
+function onClear() {
+    $('#numberOne').val('');
+    $('#numberTwo').val('');
    
-//}
-
+}
+//GETS the calculation from the server side
 function renderCalculation(){
     $.ajax({
         method:'GET',
@@ -74,14 +60,23 @@ function renderCalculation(){
         console.log(response);
         $('#answer').text(response[response.length - 1].finalResult)
     ;
+    })
+}
+//appends the calculations into an unordered list
+    function appendList() {
+        $.ajax({
+            method:'GET',
+            url: '/answer'
+        }).then((response) => {
+//dont forget to empty before appending!!
     $('#calculationList').empty();
-    for(let results of response){
-        $('#calculationlist').append(`
-        <li>${results.inputOne} ${results.operator} ${results.finalResult}</li>
+    for(let results of response)
+        $('#calculationList').append(`
+        <li>${results.inputOne} ${results.operator} ${results.inputTwo} = ${results.finalResult}</li>
         `)
-    }
-})}
-
+    })
+}
+//function for operator buttons/routed for each one
 function addition(){
     inputArray.operator = add;
 }
